1f0deb5679ea95d413af36fa266f50875d5402cb
pabagui <aguila.pablo@gmail.com>
2023-02-09 01:11:19 -0300

    fork random

--- desafio14/controllers/controllerGetRandoms.js
+++ desafio14/controllers/controllerGetRandoms.js
@@ -1,3 +1,21 @@
+import { fork } from 'child_process'
+import path from 'path'
+
+/*
+let randomNumber = Math.random()*1000
+
 export function controllerGetRandoms(req, res) {
-    res.send('todo bien en el 14')
+    res.send(process.send(randomNumber))
+}
+*/
+
+export function controllerGetRandoms(req, res) {
+    let randomNumber = fork(path.resolve(process.cwd()), 'computo.js')
+    randomNumber.on('message', resultado => {
+        if (resultado === 'listo') {
+            randomNumber.send('start')
+        } else {
+            res.json({ resultado })
+        }
+    })   
 }
